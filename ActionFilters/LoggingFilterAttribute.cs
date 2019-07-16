063539a7c82e22a639cd965c744532ac53cd70fc
fredericdasilva <52976188+fredericdasilva@users.noreply.github.com>
2019-07-16 21:14:35 +0200

    Add files via upload

---
+++ ActionFilters/LoggingFilterAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Web.Http.Controllers;
+using System.Web.Http.Tracing;
+using System.Web.Http;
+using WebApi.Helpers;
+
+
+namespace WebApi.ActionFilters
+{
+    public class LoggingFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext filterContext)
+        {
+            GlobalConfiguration.Configuration.Services.Replace(typeof(ITraceWriter), new NLogger());
+            var trace = GlobalConfiguration.Configuration.Services.GetTraceWriter();
+            trace.Info(filterContext.Request, "Controller : " + filterContext.ControllerContext.ControllerDescriptor.ControllerType.FullName + Environment.NewLine + "Action : " + filterContext.ActionDescriptor.ActionName, "JSON", filterContext.ActionArguments);
+            trace.Info(filterContext.Request, "Client @IP :" + GetIpValue(),"JSON");
+        }
+
+        //test Fred : @IP to validate
+        private string GetIpValue()
+        {
+            string ipAdd;
+            ipAdd = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+            if (string.IsNullOrEmpty(ipAdd))
+            {
+                ipAdd = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+            }
+
+            return ipAdd;
+        }
+    }
+
+}
